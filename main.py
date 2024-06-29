0951fef86e4fb509461e29fe3db6c71a9660f2c0
WickedTrad3 <jonleepy@gmail.com>
2024-06-29 19:42:54 +0800

    added new rules

--- main.py
+++ main.py
@@ -15,31 +15,28 @@ def check_folders(directory, cwd, options):
     android_manifest_found = False
 
     output = {
-            "AndroidManifest": [],
-            "SMS and Communication":{},
-            "Device Information and System Interaction":{},
-            "Network Communication": {},
-            "Contacts and Communication": {},
-            "File and Data Handling": {},
-            "Cryptography and Data Storage": {},
-            "Media and Camera": {},
-            "System and Reflection":{},
-            "User Interface and Accessibility":{},
-            "Location and Communication": {},
-            "Content Providers and Databases": {},
-            "Audio and Video": {},
-            "Bluetooth and Communication": {},
-            "File Access and Storage": {},
-            "Camera and Media": {},
-            "Accessibility and System Settings": {},
-            "Potential obfuscation": {},
-            "Ransomware": {},
-            "Logging":{},
-            "General intent usage.": {},
-            "Intent mail function.": {},
-            "Intent communication.": {},
-            "Data related intent.": {}
-        }
+        "AndroidManifest": [],
+        "SMS and Communication": {},
+        "Device Information and System Interaction": {},
+        "Network Communication": {},
+        "Contacts and Communication": {},
+        "File and Data Handling": {},
+        "Cryptography": {},
+        "Media and Camera": {},
+        "System and Reflection": {},
+        "User Interface": {},
+        "Location": {},
+        "Content Providers and Databases": {},
+        "Audio and Video": {},
+        "Bluetooth": {},
+        "Accessibility and System Settings": {},
+        "Potential obfuscation": {},
+        "Ransomware": {},
+        "Logging": {},
+        "General intent usage": {},
+        "Communication": {},
+        "Data related intent": {}
+    }
 
     json_create()
     for path, folders, files in os.walk(directory):
@@ -98,46 +95,6 @@ def decompile(directory, cwd, method, outputpath):
         process.wait()  # Wait for process to complete.
 
 def generate_html_table(data):
-    '''
-    <div class="accordion" id="accordionPanelsStayOpenExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-    '''
     count=0
     
     html = '<html><head><title>Flagged Results</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script></head><body>'
